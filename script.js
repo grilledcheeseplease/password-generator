25fbd137d36edada71f4a30a423f04df1e404909
grilledcheeseplease <lacynunnally402@gmail.com>
2022-05-22 22:29:24 -0400

    Finally got it to work!

--- script.js
+++ script.js
@@ -1,6 +1,80 @@
-// Assignment Code
+// Going to bring up the query selector
 var generateBtn = document.querySelector("#generate");
 
+// Function to generate the password
+function generatePassword () {
+    // character variables
+    var alphaLower = "abcdefghijklmnopqrstuvwxyz";
+    var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbers = "0123456789";
+    var symbols = "!@#$%^&*_+=";
+
+    //checking they run
+    console.log(alphaLower);
+    console.log(alphaUpper);
+    console.log(numbers);
+    console.log(symbols);
+    
+    // declare variable to hold password string
+    var password = "";
+
+    console.log(password);
+    // declare variable to hold password length and prompt for how long they want it to be
+    var passwordLength = window.prompt("How long do you want password to be?");
+
+    // make sure the value entered is a number if not they will be prompted again
+    while (isNaN(passwordLength)) {
+        passwordLength = window.prompt("Must be a number.");
+    }
+
+    // ensure that password is between 8 and 128 characters if not it will prompt them again
+    while (passwordLength < 8 && passwordLength < 128) {
+        passwordLength = window.prompt("Must be at least 8 characters and less than 128 characters.");
+
+    }
+
+    // variables to store the user selections for password value
+    var yesLowercase = window.confirm("Do you want lowercase letters?");
+    var yesUppercase = window.confirm("Do you want uppercase letters?");
+    var yesNumbers = window.confirm("Do you want numbers?");
+    var yesSymbols = window.confirm("Do you want symbols?");
+
+    // create a loop to prompt user for preferences and alert them if they did not choose any
+    while (!yesLowercase && !yesUppercase && !yesNumbers && !yesSymbols){
+        window.alert("Must include at least 1 special character.");
+        yesLowercase = window.confirm("Do you want lowercase letters?");
+        yesUppercase = window.confirm("Do you want uppercase letters?");
+        yesNumbers = window.confirm("Do you want numbers?");
+        yesSymbols = window.confirm("Do you want symbols?");
+}
+
+// Create a variable to help create the password to the users preferences
+var possibleChar = ""
+
+// If statements to store if the user selected yes or no
+if (yesLowercase) {
+    possibleChar += alphaLower 
+}
+
+if (yesUppercase) {
+    possibleChar += alphaUpper
+}
+
+if (yesNumbers) {
+    possibleChar += numbers
+}
+
+if (yesSymbols) {
+    possibleChar += symbols
+}
+
+//Where the random character selection magic is happening 
+for (var i = 0; i < passwordLength; i++) {
+    password += possibleChar[Math.floor(Math.random()* possibleChar.length)]
+}
+console.log(password);
+return password;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,28 +86,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// declare arrays to hold each character type
-var alphaLower = ("abcdefghijklmnopqrstuvwxyz");
-var alphaUpper = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-var numbers = ("0123456789");
-var symbols = ("!@#$%^&*-_=+");
-
-console.log(alphaLower);
-console.log(alphaUpper);
-console.log(numbers);
-console.log(symbols);
-
-
-// declare variable to hold password string
-
-
-// declare variable to hold password length
-
-// ask user if they want lowercase characters
-// ask user if they want uppercase characters
-// "" numbers
-// "" special characters
-// ask user how long they want the password to be
-
 
